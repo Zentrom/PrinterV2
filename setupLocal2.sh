4e550640f48d028cea5042a5ed5a922e1869afc2
Zentrom <26492517+Zentrom@users.noreply.github.com>
2026-06-10 21:49:01 +0000

    setup fixes

--- setupLocal2.sh
+++ setupLocal2.sh
@@ -1,8 +1,27 @@
 ollama pull llama3.2:1b
 
 cd /opt
-sudo wget "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US" -O firefox.tar.bz2
-sudo tar xjf firefox.tar.bz2
-sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+sudo wget "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US" -O firefox.tar.xz
+sudo tar -xJf firefox.tar.xz
+sudo ln -sf /opt/firefox/firefox /usr/local/bin/firefox
+
+sudo apt update
+sudo apt install -y \
+    libgtk-3-0 \
+    libdbus-glib-1-2 \
+    libasound2t64 \
+    libxt6 \
+    libx11-xcb1 \
+    libxcb-shm0 \
+    libxcb-dri3-0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libpango-1.0-0 \
+    libcairo2 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0
 
 firefox --version
