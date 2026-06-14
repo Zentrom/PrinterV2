1505a3c17d82c62b1d18d0c4b572de1f56e59e1f
user <26492517+Zentrom@users.noreply.github.com>
2026-06-14 19:30:44 +0000

    minor config change

--- src/classes/YouTube.py
+++ src/classes/YouTube.py
@@ -616,8 +616,8 @@ class YouTube:
             generator = lambda txt: TextClip(
                 text=txt,
                 font=os.path.join(get_fonts_dir(), get_font()),
-                font_size=100,
-                color="#FFFF00",
+                font_size=80,
+                color="#00FF00",
                 stroke_color="black",
                 stroke_width=5,
                 size=(final_clip.w, None),
@@ -646,6 +646,8 @@ class YouTube:
         final_clip.write_videofile(combined_image_path, threads=threads)
 
         success(f'Wrote Video to "{combined_image_path}"')
+
+        self.images = []
 
         return combined_image_path
 
